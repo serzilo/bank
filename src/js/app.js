eac18c93c141349810d119c19345cdc82fe49af9
serzilo <designkota@mail.ru>
2017-02-09 11:42:50 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,10 @@
 var $ = require('jquery');
 var _ = require("underscore");
+
+var $app = $('#app');
+
+$app.on('click', '.js-toggle-list', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('toggle-list__button_active');
+});
