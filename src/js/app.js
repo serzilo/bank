39314b57458365d1216105f36a287186f250c10e
serzilo <designkota@mail.ru>
2017-02-10 18:12:32 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,23 @@ $app.on('click', '.js-toggle-list', function(e) {
 
     $(this).toggleClass('toggle-list__button_active');
 });
+
+$app.on('click', '.js-show-popup', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var popupid = $(this).data().popupid;
+
+    $('#' + popupid).show();
+    $('body').addClass('popup_open');
+});
+
+$app.on('click', '.js-hide-popup', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var popupid = $(this).data().popupid;
+
+    $('#' + popupid).hide();
+    $('body').removeClass('popup_open');
+});
