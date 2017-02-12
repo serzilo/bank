406c775377a326135c086eb3141e89e199c5ba6a
serzilo <designkota@mail.ru>
2017-02-12 18:45:31 +0300

    scroll fix

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,10 @@ $app.on('click', '.js-show-popup', function(e) {
 
     var popupid = $(this).data().popupid;
 
+    if (get_scroll('Height') == true) {
+        $('body').css({ 'padding-right': get_scroll_width()});
+    }
+
     $('#' + popupid).show();
     $('body').addClass('popup_open');
 });
@@ -26,5 +30,34 @@ $app.on('click', '.js-hide-popup', function(e) {
     var popupid = $(this).data().popupid;
 
     $('#' + popupid).hide();
-    $('body').removeClass('popup_open');
+    $('body').removeClass('popup_open').css({ 'padding-right': 0});
 });
+
+function get_scroll(a) {
+    var d = document,
+        b = d.body,
+        e = d.documentElement,
+        c = "client" + a;
+    a = "scroll" + a;
+    return /CSS/.test(d.compatMode)? (e[c]< e[a]) : (b[c]< b[a])
+};
+
+function get_scroll_width() {
+    // создадим элемент с прокруткой
+    var div = document.createElement('div');
+
+    div.style.overflowY = 'scroll';
+    div.style.width = '50px';
+    div.style.height = '50px';
+
+    // при display:none размеры нельзя узнать
+    // нужно, чтобы элемент был видим,
+    // visibility:hidden - можно, т.к. сохраняет геометрию
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    var scrollWidth = div.offsetWidth - div.clientWidth;
+    document.body.removeChild(div);
+
+    return scrollWidth;
+}
